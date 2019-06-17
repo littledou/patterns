8ccd52c0a2883a9dc5033624ac4b468ef7655716
dou <hongbin.dou@gmail.com>
2019-06-17 15:51:25 +0800

    采用扩展Binder的方式实现IBinder

--- app/src/main/java/cn/readsense/servicetest/MainActivity.kt
+++ app/src/main/java/cn/readsense/servicetest/MainActivity.kt
@@ -6,47 +6,52 @@ import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
-import android.os.Parcel
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+
+    var binder: ManualCalculatorService.LocalBinder? = null
+    var manualCalculatorService: ManualCalculatorService? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Intent()
-        intent.component = ComponentName("cn.readsense.servicetest", "cn.readsense.servicetest.ManualCalculatorService")
+        val intent = Intent(this, ManualCalculatorService::class.java)
+//        intent.component = ComponentName("cn.readsense.servicetest", "cn.readsense.servicetest.ManualCalculatorService")
 
-        bindService(intent, object : ServiceConnection {
+        var conn = object : ServiceConnection {
             override fun onServiceDisconnected(name: ComponentName?) {
 
             }
 
             override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
                 llog("servicetest running")
+                binder = service as ManualCalculatorService.LocalBinder?
 
-                val data = Parcel.obtain()
-                val reply = Parcel.obtain()
+                manualCalculatorService = binder?.service
 
-                data.writeInt(1)
-                data.writeInt(2)
-
-                service?.transact(1000, data, reply, 0)
-
-                val result = reply.readInt()
-                data.recycle()
-                reply.recycle()
-
-                llog("reply: $result")
 
             }
-        }, Context.BIND_AUTO_CREATE)
+        }
+
+        bindService(intent, conn, Context.BIND_AUTO_CREATE)
+
+
+        Thread(Runnable {
+            Thread.sleep(1000)
+            while(manualCalculatorService?.count!! <=20){
+
+                llog("count : "+manualCalculatorService?.count)
+                Thread.sleep(1000)
+            }
+            unbindService(conn)
+
+        }).start()
 
     }
 
     fun llog(str: String) {
-        Log.d("servicetest", str)
+        println("activity: $str")
     }
 }
--- app/src/main/java/cn/readsense/servicetest/ManualCalculatorService.kt
+++ app/src/main/java/cn/readsense/servicetest/ManualCalculatorService.kt
@@ -4,28 +4,72 @@ import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
-import android.os.Parcel
-import android.os.RemoteException
 
+
+/**
+ * 采用扩展Binder方式来实现IBinder
+ */
 class ManualCalculatorService : Service() {
 
-    override fun onBind(intent: Intent): IBinder {
-        return object : Binder() {
-            @Throws(RemoteException::class)
-            override fun onTransact(code: Int, data: Parcel, reply: Parcel?, flags: Int): Boolean {
 
-                when (code) {
-                    1000 -> {
-                        val num1 = data.readInt()
-                        val num2 = data.readInt()
-                        println("get $num1 , $num2")
-                        reply?.writeInt(num1 + num2)
-                        return true
-                    }
-                }
+    internal var binder = LocalBinder()
 
-                return super.onTransact(code, data, reply, flags)
-            }
-        }
+    var count = 0
+
+    var quit = false
+
+    inner class LocalBinder : Binder() {
+        // 声明一个方法，getService。（提供给客户端调用）
+        // 返回当前对象LocalService,这样我们就可在客户端端调用Service的公共方法了
+        internal val service: ManualCalculatorService
+            get() = this@ManualCalculatorService
     }
+
+    override fun onBind(intent: Intent): IBinder {
+        println("onBind invoke")
+        return binder
+
+    }
+
+    override fun onCreate() {
+        println("onCreate invoke")
+        super.onCreate()
+
+        Thread(Runnable {
+
+            while (!quit) {
+                Thread.sleep(1000)
+
+                count++
+
+                println("count: $count")
+            }
+
+        }).start()
+    }
+
+
+    override fun onStart(intent: Intent?, startId: Int) {
+        println("onStart invoke")
+        super.onStart(intent, startId)
+        stopSelf()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        println("onStartCommand invoke")
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        println("onUnbind invoke")
+        return super.onUnbind(intent)
+    }
+
+    override fun onDestroy() {
+        println("onDestroy invoke")
+        super.onDestroy()
+        quit = true
+    }
+
+
 }
