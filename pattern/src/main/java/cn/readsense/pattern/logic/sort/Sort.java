7c78d75ed706dc69b8c88cd735c8570f60cb5c14
dou <hongbin.dou@gmail.com>
2019-08-26 14:00:32 +0800

    选择排序

--- pattern/src/main/java/cn/readsense/pattern/logic/sort/Sort.java
+++ pattern/src/main/java/cn/readsense/pattern/logic/sort/Sort.java
@@ -6,16 +6,16 @@ public class Sort {
 
     /**
      * 两个相邻数相比较，较大数下沉，较小数上升
+     * 交换次数比选择排序要多，但是优化后的冒泡排序遍历次数可能会比选择排序少
+     * 也就是说，逆序复杂度最高，正续复杂度最低，但是选择排序无关正逆
      *
      * @param arr
      */
     public static void bubbleSort(int[] arr) {
         int temp;
         int length = arr.length;
-        int count = 0;
 
-        for (int i = 0; i < length; i++) {
-
+        for (int i = 0; i < length - 1; i++) {
             boolean needExchange = false;
             for (int j = i + 1; j < length; j++) {
 
@@ -25,19 +25,57 @@ public class Sort {
                     arr[j] = temp;
                     needExchange = true;
                 }
-                count++;
-
-                System.out.println(Arrays.toString(arr) + " needExchange:" + needExchange);
             }
+            System.out.println(Arrays.toString(arr) + " needExchange:" + needExchange);
+            if (!needExchange) break;
+        }
+        System.out.println("\n");
+
+//        for (int i = 0; i < arr.length - 1; i++) {   //表示趟数，一共arr.length-1次。
+//            for (int j = arr.length - 1; j > i; j--) {
+//
+//                if (arr[j] < arr[j - 1]) {
+//                    temp = arr[j];
+//                    arr[j] = arr[j - 1];
+//                    arr[j - 1] = temp;
+//                }
+//            }
+//            System.out.println(Arrays.toString(arr));
+//        }
+
+    }
+
+    /**
+     * 选择出最小数放在最前面
+     */
+    public static void selectSort(int arr[]) {
+        int temp;
+        int length = arr.length;
+
+        for (int i = 0; i < length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < length; j++) {
+                if (arr[minIndex] > arr[j]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+            System.out.println(Arrays.toString(arr));
         }
     }
 
     public static void main(String args[]) {
 
         int arr[] = new int[]{
-                29, 65, 34, 24, 56, 78, 97, 4, 32, 56
+                29, 65, 34, 24, 56, 4, 32, 56, 78, 97
         };
-        bubbleSort(arr);
 
+//        bubbleSort(arr);
+
+        selectSort(arr);
     }
 }
