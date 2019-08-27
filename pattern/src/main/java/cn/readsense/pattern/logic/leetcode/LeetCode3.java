7c12a31bf4e29f5f673e42dc09f9dd5d777c99eb
dou <hongbin.dou@gmail.com>
2019-08-28 01:32:13 +0800

    LC3：快速一点的办法

--- pattern/src/main/java/cn/readsense/pattern/logic/leetcode/LeetCode3.java
+++ pattern/src/main/java/cn/readsense/pattern/logic/leetcode/LeetCode3.java
@@ -1,7 +1,6 @@
 package cn.readsense.pattern.logic.leetcode;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度
@@ -10,12 +9,13 @@ public class LeetCode3 {
 
 
     public static void main(String args[]) {
-
+        lengthOfLongestSubstring1("abcabcbb");
     }
 
 
     /**
      * 暴力法，时间会超出
+     *
      * @param s
      * @return
      */
@@ -42,6 +42,33 @@ public class LeetCode3 {
         }
 
         return longest;
+    }
+
+    public static int lengthOfLongestSubstring1(String s) {
+        Set<Character> set = new LinkedHashSet<>();
+
+        final char[] chars = s.toCharArray();
+
+        int targetPosition = 0;
+        int nextLength;
+        int maxLength = 0;
+        for (int i = 0; i < chars.length; i++) {
+
+            nextLength = set.size() + 1;
+            final char aChar = chars[i];
+            set.add(aChar);
+
+            while (set.size() != nextLength) {
+                set.remove(chars[targetPosition]);
+                targetPosition++;
+                nextLength = set.size() + 1;
+                set.add(aChar);
+            }
+            if (maxLength < nextLength) maxLength = nextLength;
+
+        }
+        return maxLength;
+
     }
 
 }
